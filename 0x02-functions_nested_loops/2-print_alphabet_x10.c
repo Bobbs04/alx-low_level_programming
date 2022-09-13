dd74030b34348338ae1b4bf6b1f3e652cb072a7a
Bobbs04 <win3ldestiny@gmai.com>
2022-09-13 16:54:09 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - prints the alphabet x10
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int wy;
+	int wy2;
+
+	for (wy = 0; wy <= 9; wy++)
+	{
+		for (wy2 = 'a'; wy2 <= 'z'; wy2++
+		{
+
+		_putchar(wy2);
+		}
+	
+		_putchar('\n');
+
+          }
+}
